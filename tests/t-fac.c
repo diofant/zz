b7f1f540fdad55c96aa0dc37ba51caace6354969
Sergey B Kirpichev <skirpichev@gmail.com>
2025-10-12 05:51:05 +0300

    Add some tests

---
+++ tests/t-fac.c
@@ -0,0 +1,58 @@
+/*
+    Copyright (C) 2024, 2025 Sergey B Kirpichev
+
+    This file is part of the ZZ Library.
+
+    The ZZ Library is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License (LGPL) as
+    published by the Free Software Foundation; either version 3 of the License,
+    or (at your option) any later version.  See
+    <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <time.h>
+
+#include "zz.h"
+
+void check_fac_outofmem()
+{
+    for (size_t i = 0; i < 7; i++) {
+        uint64_t x = 12811 + (rand() % 12173);
+        zz_t mx;
+
+        if (zz_init(&mx)) {
+            abort();
+        }
+        while (1) {
+            zz_err r = zz_fac(x, &mx);
+
+            if (r != ZZ_OK) {
+                if (r == ZZ_MEM) {
+                    break;
+                }
+                abort();
+            }
+            x *= 2;
+        }
+        zz_clear(&mx);
+    }
+}
+
+int main(void)
+{
+    srand(time(NULL));
+    zz_setup(NULL);
+
+    struct rlimit new;
+
+    new.rlim_max = new.rlim_cur = 32*1000*1000;
+    if (setrlimit(RLIMIT_AS, &new)) {
+        fprintf(stderr, "can't set memory limits\n");
+        return 1;
+    }
+    check_fac_outofmem();
+    return 0;
+}
---
+++ tests/t-mul.c
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2024, 2025 Sergey B Kirpichev
+
+    This file is part of the ZZ Library.
+
+    The ZZ Library is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License (LGPL) as
+    published by the Free Software Foundation; either version 3 of the License,
+    or (at your option) any later version.  See
+    <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <time.h>
+
+#include "zz.h"
+
+void check_square_outofmem()
+{
+    for (size_t i = 0; i < 7; i++) {
+        int64_t x = 49846727467293 + rand();
+        zz_t mx;
+
+        if (zz_init(&mx) || zz_from_i64(x, &mx)) {
+            abort();
+        }
+        while (1) {
+            zz_err r = zz_mul(&mx, &mx, &mx);
+
+            if (r != ZZ_OK) {
+                if (r == ZZ_MEM) {
+                    break;
+                }
+                abort();
+            }
+        }
+        zz_clear(&mx);
+    }
+}
+
+int main(void)
+{
+    srand(time(NULL));
+    zz_setup(NULL);
+
+    struct rlimit new;
+
+    new.rlim_max = new.rlim_cur = 32*1000*1000;
+    if (setrlimit(RLIMIT_AS, &new)) {
+        fprintf(stderr, "can't set memory limits\n");
+        return 1;
+    }
+    check_square_outofmem();
+    return 0;
+}
