229b58df673d549f3e7281d392238c8a2cfabd21
Sergey B Kirpichev <skirpichev@gmail.com>
2026-03-14 08:05:41 +0300

    Reimplement zz_powm
    Closes #53

--- mpn.h
+++ mpn.h
@@ -23,4 +23,25 @@ void * mpn_export(void *data, size_t *countp, int order,
 void mpn_import(mp_ptr zp, mp_size_t *zsize, size_t count, int order,
                 size_t size, int endian, size_t nail, const void *data);
 
+/* Compute r = b^e mod m.  Requires that m is odd and e > 1.
+   Uses scratch space at tp of MAX(mpn_binvert_itch(n), 2n) limbs. */
+void mpn_powm(mp_limb_t *rp, const mp_limb_t *bp, mp_size_t bn,
+              const mp_limb_t *ep, mp_size_t en, const mp_limb_t *mp,
+              mp_size_t n, mp_limb_t *tp);
+mp_size_t mpn_binvert_itch(mp_size_t n);
+
+/* Compute r = b^e mod B^n, B is the limb base.
+   Requires normalized e.  Uses scratch space of 3n words in tp. */
+void mpn_powlo(mp_limb_t *rp, const mp_limb_t *bp,
+               const mp_limb_t *ep, mp_size_t en, mp_size_t n,
+               mp_limb_t *tp);
+
+/* Compute r = u^(-1) mod B^n, B is the limb base. */
+void mpn_binvert(mp_limb_t *rp, const mp_limb_t *up, mp_size_t n,
+                 mp_limb_t *scratch);
+
+/* Multiply two n-limb numbers and return the low n limbs of their products. */
+void mpn_mullo_n(mp_limb_t *rp, const mp_limb_t *xp,
+                 const mp_limb_t *yp, mp_size_t n);
+
 #endif /* MPN_H */
